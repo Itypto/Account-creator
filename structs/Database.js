9514088e02e57bd0a3e5445763ac9348e9e8752a
Itypto <filipvannman95@gmail.com>
2026-01-15 21:45:52 +0100

    Refactor MongoDB connection logging messages

--- structs/Database.js
+++ structs/Database.js
@@ -6,11 +6,12 @@ const config = require(path.join(__dirname, "../config/Config.json"));
 async function connectDB() {
     try {
         await mongoose.connect(config.mongodb.database);
-        console.log("✅ Connected to MongoDB!");
+        console.log("Connected to MongoDB!");
     } catch (err) {
-        console.error("❌ MongoDB connection error:", err);
+        console.error("MongoDB connection error:", err);
         process.exit(1); 
     }
 }
+
 
 module.exports = connectDB;
